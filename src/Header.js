cc1d7cda04a18a300e2838368a10233cfeff3437
sebastian coll <collsebastian@hotmail.com>
2021-07-25 14:49:12 -0300

    Segundo Trabajo Practico React JS

---
+++ src/Header.js
@@ -0,0 +1,18 @@
+const Header = () => 
+
+
+            <header>
+
+                    <h1>TIENDA ECOMMERCE</h1>
+
+                    <ul>
+                        <li><a href="#">EMPRESA</a></li>
+                        <li><a href="#">PRODUCTOS</a></li>
+                        <li><a href="#">TECNOLOGIAS</a></li>
+                        <li><a href="#">CONTACTO</a></li>
+                    </ul>
+
+            </header>   
+            
+
+            export default Header
